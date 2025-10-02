eda36a802f033d77e6096a43a365dd3b34870327
tamara joy <tamaraxlx@gmail.com>
2025-10-02 16:07:53 +0700

    benerin isi main.py di backend oleh tamara

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import Base, engine
 from routers import auth
 
 Base.metadata.create_all(bind=engine)
 app = FastAPI()
+
+# Izinkan akses dari frontend (Live Server)
+origins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 
 @app.get("/")
